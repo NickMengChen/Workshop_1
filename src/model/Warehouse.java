9aa3164fc04e9e6282579ae8bf14dda5cd7b953b
Chen Meng <94532161@qq.com>
2019-04-13 15:21:11 +0800

    [Chen Meng] add check method & isExist method

--- src/model/Warehouse.java
+++ src/model/Warehouse.java
@@ -2,29 +2,74 @@ package model;
 
 import model.Material;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Warehouse {
 
-    private Map<Material, Integer> Materials;
+    private HashMap<Material, Integer> Materials = new HashMap<>();
 
     public Map<Material, Integer> getMaterials() {
         return Materials;
     }
 
     public void setMaterials(Map<Material, Integer> materials) {
-        Materials = materials;
+        //Materials = materials;
     }
 
-    public void addMerials()
+    public void addMerials(Material material, int stock)
     {
 
 
     }
 
-    public boolean checkavaliable(){
+    public boolean checkIsExist(Material material){
 
-        return true;
+        Iterator<Map.Entry<Material, Integer>>
+                iterator = Materials.entrySet().iterator();
+        // flag to store result
+        boolean isKeyPresent = false;
+
+        // Iterate over the HashMap
+        while (iterator.hasNext()) {
+
+            // Get the entry at this iteration
+            Map.Entry<Material, Integer>
+                    entry
+                    = iterator.next();
+
+            // Check if this key is the required key
+            if (material == entry.getKey()) {
+
+                isKeyPresent = true;
+            }
+        }
+        return isKeyPresent;
+
+    }
+
+    public boolean checkavaliable(Material material){
+
+        Iterator<Map.Entry<Material, Integer>>
+                iterator = Materials.entrySet().iterator();
+        // Iterate over the HashMap
+        while (iterator.hasNext()) {
+
+            // Get the entry at this iteration
+            Map.Entry<Material, Integer>
+                    entry
+                    = iterator.next();
+
+            // Check if this key is the required key
+            if (material == entry.getKey()) {
+
+                if (entry.getValue() > 0)
+                    return true;
+                else
+                    return false;
+            }
+        }
     }
 
     public void PrintStorgeReport() throws Exception{

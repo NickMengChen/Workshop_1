4ab23deb632420019439f587d900703c13f66af2
Chen Meng <94532161@qq.com>
2019-04-16 14:37:26 +0800

    [TDD] Chen Meng: Init the TEST for FizzBuzz

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,7 @@ public class Main {
     public static void main (String [] args)
     {
         Warehouse warehouse = new Warehouse();
-//        List<Material> materials;
-////        warehouse
-//        private String name;
-//        private double price;
-//        private String type;
+
         Material ma1 = new Material("Iron", 1.5, "hahaha");
         Material ma2 = new Material("Gold", 1.5, "lalala");
         //warehouse.CheckStroge();
